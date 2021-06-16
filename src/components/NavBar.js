6d301c51b4b7aa2a8c44c7f82ec80a4bd286fff1
dprincecoder <78590038+dprincecoder@users.noreply.github.com>
2021-06-16 10:14:12 +0100

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,11 +26,6 @@ function NavBar() {
                          </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/blogs" exact activeClassName="active">
-                           Blogs
-                         </NavLink>
-                    </li>
-                    <li className="nav-item">
                         <NavLink to="/contact" exact activeClassName="active">
                           Contact
                          </NavLink>
