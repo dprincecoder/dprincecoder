da94528be40886fedefc99d5e9ead59414fc7887
dprincecoder <78590038+dprincecoder@users.noreply.github.com>
2021-06-24 13:41:56 +0100

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -21,14 +21,14 @@ function AboutPage() {
                   <SkillsSetion skill={'React Js'} progress={'70%'} width={'70%'}/>
                   <SkillsSetion skill={'Node Js'} progress={'60%'} width={'60%'}/>
                   <SkillsSetion skill={'Express Js'} progress={'55%'} width={'55%'}/>
-                  <SkillsSetion skill={'Github'} progress={'65%'} width={'65%'}/>
+                  <SkillsSetion skill={'Github'} progress={'75%'} width={'75%'}/>
                   <SkillsSetion skill={'Html'} progress={'95%'} width={'95%'}/>
                   <SkillsSetion skill={'Css'} progress={'90%'} width={'90%'}/>
                   <SkillsSetion skill={'Git Bash'} progress={'50%'} width={'50%'}/>
                   <SkillsSetion skill={'Version Control'} progress={'70%'} width={'70%'}/>
                   <SkillsSetion skill={'Python'} progress={'40%'} width={'40%'}/>
-                  <SkillsSetion skill={'Sass'} progress={'75%'} width={'75%'}/>
-                  <SkillsSetion skill={'Firebase'} progress={'40%'} width={'40%'}/>
+                  <SkillsSetion skill={'Scss'} progress={'75%'} width={'75%'}/>
+                  <SkillsSetion skill={'Firebase'} progress={'70%'} width={'70%'}/>
                   <SkillsSetion skill={'MongoDB'} progress={'55%'} width={'55%'}/>
             </div>
             <div className="services-container">
